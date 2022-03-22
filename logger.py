28f55fc4e6159ea7a63fdb01fa70326fc97fe715
君柏柏 <8089140+juniper-cypress@user.noreply.gitee.com>
2022-03-22 19:48:11 +0800

    feat:继承logging的类

--- logger.py
+++ logger.py
@@ -3,13 +3,13 @@ import os
 import time
 
 
-class Logger:
-    __slots__ = ["_logger", "_stream_handler", "_formatter", "_file_handler"]
+class Logger(logging.Logger):
+    __slots__ = ["_stream_handler", "_formatter", "_file_handler"]
 
-    def __init__(self, name, log_save_path="./log", log_save_name=f"{time.strftime('%Y%m%d', time.localtime(time.time()))}.log",
+    def __init__(self, name, log_save_path="./log",
+                 log_save_name=f"{time.strftime('%Y%m%d', time.localtime(time.time()))}.log",
                  level=logging.INFO):
-        self._logger = logging.getLogger(name)
-        self._logger.setLevel(level)
+        super(Logger, self).__init__(name, level)
         self._stream_handler = logging.StreamHandler()
         if not os.path.exists(log_save_path):
             os.mkdir(log_save_path)
@@ -20,34 +20,14 @@ class Logger:
             "%(asctime)s %(name)s %(levelname)s %(filename)s-Process:%(process)d-Thread:%(thread)d-%(funcName)s-%(lineno)d: %(message)s")
         self._stream_handler.setFormatter(self._formatter)
         self._file_handler.setFormatter(self._formatter)
-        self._logger.addHandler(self._stream_handler)
-        self._logger.addHandler(self._file_handler)
+        self.addHandler(self._stream_handler)
+        self.addHandler(self._file_handler)
 
-    @property
-    def logger(self):
-        return self._logger
 
-    @property
-    def d(self):
-        return self._logger.debug
-
-    @property
-    def i(self):
-        return self._logger.info
-
-    @property
-    def w(self):
-        return self._logger.warning
-
-    @property
-    def e(self):
-        return self._logger.error
 
 if __name__ == '__main__':
     log = Logger(__name__)
-    log.d("12313")
-    log.i({"12313"})
-    log.w(["12313"])
-    log.e(("12313",))
-
-
+    log.debug("12313")
+    log.info({"12313"})
+    log.warning(["12313"])
+    log.error(("12313",))
